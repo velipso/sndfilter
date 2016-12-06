ad26e0527566db97425c7a232c60f70784e5ef75
Sean <voidqk@users.noreply.github.com>
2016-12-06 10:53:23 -0600

    build and license

---
+++ src/main.c
@@ -0,0 +1,12 @@
+// (c) Copyright 2016, Sean Connelly (@voidqk), http://syntheti.cc
+// MIT License
+// Project Home: https://github.com/voidqk/sndfilter
+
+// main entry point, responsible for parsing command line parameters
+
+#include <stdio.h>
+
+int main(int argc, char **argv){
+	printf("hello, world\n");
+	return 0;
+}
